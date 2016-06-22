ff194df3e5fe9a8231b064808f0d83a9635badad
bowei <bowei.han100@gmail.com>
2016-06-22 18:28:01 -0400

    added validation for datetime field so that user can't overbook at any time (only valid for seconds at the moment

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,30 @@
 class Reservation < ActiveRecord::Base
   belongs_to :restaurant
+
+  validates :number_of_guests, :email, presence: true
+  validate :reservation_time_passes
+
+  def reservation_time_passes
+    taken_spots = Reservation.where(reservation_time: "#{self.reservation_time}").count
+
+
+    p Restaurant.find(self.restaurant_id).max_occupancy
+    p taken_spots
+    p self.number_of_guests
+    p (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
+      self.number_of_guests))
+    p (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
+      self.number_of_guests)) < 0
+
+    if (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
+      self.number_of_guests)) < 0
+      errors.add(:reservation_time, "this time is invalid") unless
+        false
+    else
+      errors.add(:reservation_time, "this time is invalid") unless
+        true
+    end
+
+  end
+
 end
