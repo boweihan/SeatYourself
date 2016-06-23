5e91ab5ccb1138324a801a5a81f5eaeffa66a7f5
bowei <bowei.han100@gmail.com>
2016-06-23 13:58:59 -0400

    changed reservation.rb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,15 +5,13 @@ class Reservation < ActiveRecord::Base
   validate :reservation_time_passes
 
   def reservation_time_passes
-    taken_spots = Reservation.where("reservation_time = reservation_time").count
-
+    taken_spots = Reservation.where("restaurant_id = ?", self.restaurant_id).where("reservation_time = ?", self.reservation_time).sum(number_of_guests)
 
     if (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
       self.number_of_guests)) < 0
       errors.add(:reservation_time, "this time is invalid") unless
         false
     else
-      errors.add(:reservation_time, "this time is invalid") unless
         true
     end
 
