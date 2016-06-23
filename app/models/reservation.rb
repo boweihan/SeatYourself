e9544d90e55d24fda786fcab6f575b9adad8b350
bowei <bowei.han100@gmail.com>
2016-06-23 14:30:54 -0400

    generated user model and added login page

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,17 +4,12 @@ class Reservation < ActiveRecord::Base
   validates :number_of_guests, :email, presence: true
   validate :reservation_time_passes
 
-  def reservation_time_passes
-    taken_spots = Reservation.where("restaurant_id = ?", self.restaurant_id).where("reservation_time = ?", self.reservation_time).sum(number_of_guests)
+def reservation_time_passes
+  taken_spots = Reservation.where("restaurant_id = ?", self.restaurant_id).where("reservation_time = ?", self.reservation_time).sum(:number_of_guests)
 
-    if (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
-      self.number_of_guests)) < 0
-      errors.add(:reservation_time, "this time is invalid") unless
-        false
-    else
-        true
-    end
-
+  if (Restaurant.find(self.restaurant_id).max_occupancy - (taken_spots +
+    self.number_of_guests)) < 0
+    errors.add(:reservation_time, "this time is invalid")
   end
-
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622205810) do
+ActiveRecord::Schema.define(version: 20160623181811) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "number_of_guests"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20160622205810) do
     t.string   "website"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "phone"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
