b71c46b4435b2dfbfb7660efdc0b6ff412c456fc
Brian Harrison <brianharrison@rogers.com>
2016-06-23 16:33:25 -0400

    Migration file added for restaurants:cuisine

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623181811) do
+ActiveRecord::Schema.define(version: 20160623202207) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "number_of_guests"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160623181811) do
     t.string   "website"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "cuisine"
   end
 
   create_table "users", force: :cascade do |t|
