dc5be4e52f563ba08cdfc39767e7bf48ceee0c90
bowei <bowei.han100@gmail.com>
2016-06-23 17:17:50 -0400

    can't have negative guests

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,17 +7,13 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
   end
 
-  def new
-    @reservation = Reservation.find(params[:id])
-  end
-
   def create
     @reservation = @restaurant.reservations.build(reservation_params)
 
     if @reservation.save
-      redirect_to restaurant_reservation_path(@reservation.restaurant_id, @reservation), notice: "Thank you for your reservation!"
+      redirect_to restaurants_url, notice: "Thank you for your reservation!"
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
