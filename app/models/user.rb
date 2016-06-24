e4cb2ce8cd35455a198d48d201cf962f96c1e2ca
jonmio <jon.mio@hotmail.com>
2016-06-24 14:56:16 -0400

    Added email uniquness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, :email, :phone, presence: true
+  validates :email, uniqueness: true
 
   has_many :reservations
   has_many :restaurants, through: :reservations
